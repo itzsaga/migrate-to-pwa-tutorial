a2d37a5ffb2dd2a1631531a5a3c5ae67fed160aa
Seth <sethalexander@pm.me>
2018-12-14 12:44:47 -0600

    Add assets to cache

--- work/sw.js
+++ work/sw.js
@@ -7,3 +7,24 @@ self.addEventListener("push", function(event) {
     })
   );
 });
+
+screen.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open("the-magic-cache").then(function(cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/dragon.html",
+        "/faq.html",
+        "/manifest.json",
+        "/background.jpeg",
+        "/construction.gif",
+        "/dragon.png",
+        "/logo.png",
+        "/site.js",
+        "/dragon.js",
+        "/styles.css"
+      ]);
+    })
+  );
+});
