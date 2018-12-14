36af8e378d5f628e74419ecff259097ec6009714
Seth <sethalexander@pm.me>
2018-12-14 12:54:31 -0600

    Intercept external url fetch calls

--- work/sw.js
+++ work/sw.js
@@ -1,6 +1,16 @@
 // version: I'm nearly finished the codelab woo!
 
 self.addEventListener("fetch", function(event) {
+  if (event.request.url == "https://dragon-server.appspot.com") {
+    console.info("responding to dragon server fetch with Service Worker! 🤓");
+    event.respondWith(
+      fetch(event.request).catch(function(e) {
+        let out = { Gold: 1, Size: -1, Actions: [] };
+        return new Response(JSON.stringify(out));
+      })
+    );
+    return;
+  }
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
