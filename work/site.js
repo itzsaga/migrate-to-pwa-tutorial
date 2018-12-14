70da1ca8e82080ffc0809016ae6f273f211ba43d
Seth <sethalexander@pm.me>
2018-12-14 12:36:56 -0600

    Subscribe to push

--- work/site.js
+++ work/site.js
@@ -11,8 +11,34 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+const publicKey =
+  "BCI3r7zUsdb1WBJydTiBRnSvBYF5mcEzgajzfPNt3kNk1JN22rwaSbmE3phLKFvRqxXU_0KB7ANcf7lwsUm_hXI";
 
 navigator.serviceWorker &&
   navigator.serviceWorker.register("./sw.js").then(function(registration) {
     console.log("Excellent, registered with scope: ", registration.scope);
+  });
+
+navigator.serviceWorker &&
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+    serviceWorkerRegistration.pushManager
+      .getSubscription()
+      .then(function(subscription) {
+        if (subscription) {
+          console.info("Got existing", subscription);
+          window.subscription = subscription;
+          return;
+        }
+
+        const applicationServerKey = urlB64ToUint8Array(publicKey);
+        serviceWorkerRegistration.pushManager
+          .subscribe({
+            userVisibleOnly: true,
+            applicationServerKey
+          })
+          .then(function(subscription) {
+            console.info("Newly subscribed to push!", subscription);
+            window.subscription = subscription;
+          });
+      });
   });
