554adf3d0d9028ff32ba24183bbde27233fc0e38
Seth <sethalexander@pm.me>
2018-12-14 12:42:46 -0600

    Add push functionality to service worker

--- work/sw.js
+++ work/sw.js
@@ -1,1 +1,9 @@
 self.addEventListener("fetch", function(event) {});
+
+self.addEventListener("push", function(event) {
+  event.waitUntil(
+    self.registration.showNotification("Got Push?", {
+      body: "Push Message received"
+    })
+  );
+});
